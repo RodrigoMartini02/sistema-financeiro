cab58a4e2b5eded4a184311f0c306536f0e4ff15
Rodrigo <martini.rodrigo1992@gmail.com>
2026-03-21 09:43:07 -0300

    Melhorias

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Sistema de Controle Financeiro
 // ================================================================
 
-const CACHE_VERSION = 'v39';
+const CACHE_VERSION = 'v40';
 const CACHE_STATIC  = `sf-static-${CACHE_VERSION}`;
 
 // Assets estáticos que serão cacheados no install
